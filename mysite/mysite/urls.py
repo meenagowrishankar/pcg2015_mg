f46cf795b7910d15cc44eccaf9102bcf1413688b
transits1800 <meenambika@gmail.com>
2015-03-23 21:23:21 -0700

    init commit

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+	url(r'^polls/', include('polls.urls', namespace="polls")),
+	
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from polls.models import Question
+from polls.models import Choice, Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None,               {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+
+	]
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+	inlines = [ChoiceInline]
+	list_filter = ['pub_date']
+	search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
 # Register your models here.
